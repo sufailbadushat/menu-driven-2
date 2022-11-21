f04c245b93f15a3b5e16c4cbe761ce7bf4b12a97
sufailbadushat <sufailbadushat@gamil.com>
2022-11-21 12:46:24 +0530

    largest of 3 numbers added

--- src/Main.java
+++ src/Main.java
@@ -29,7 +29,7 @@ public class Main {
                     checkEven();
                     break;
                 case 5:
-                    reverse():
+                    reverse();
                     break;
                 case 6:
                     System.exit(0);
@@ -38,7 +38,25 @@ public class Main {
         }
 
     }
-
+    static void largest(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter 3 numbers:");
+        double num1=scan.nextDouble();
+        double num2=scan.nextDouble();
+        double num3=scan.nextDouble();
+        if(num1 > num2 && num1 > num3)
+        {
+            System.out.println("Largest number is:"+num1);
+        }
+        else if(num2 > num3)
+        {
+            System.out.println("Largest number is:"+num2);
+        }
+        else
+        {
+            System.out.println("Largest number is:"+num3);
+        }
+    }
 
 }
 
